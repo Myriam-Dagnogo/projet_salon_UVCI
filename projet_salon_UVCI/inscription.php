0e9fe09105252e79575f642c6f322ac25020c84f
Dagnogo  Myriam Y. R. S <88391281+Myriam-Dagnogo@users.noreply.github.com>
2022-09-09 14:29:26 +0000

    Add files via upload

---
+++ projet_salon_UVCI/inscription.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contact us</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">
+</head>
+<body>
+	<div class="container">
+		<div class="contact-box">
+			<div class="left"></div>
+			<div class="right">
+				<h2>Formulaire d'inscription</h2>
+				<input type="text" class="field" placeholder="Username">
+				<input type="text" class="field" placeholder="Email">
+				<input type="text" class="field" placeholder="Contact">
+				<input type="text" class="field" placeholder="Mot de passe">
+				
+				<button class="btn">S'inscrire</button>
+				<h4>Déjà inscrit? <a href="">Connectez-vous</a></h4>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
